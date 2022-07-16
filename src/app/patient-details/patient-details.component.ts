810aee0b8073a6642679df376b1e6652c2c72219
sowmya2014 <75194242+sowmya2014@users.noreply.github.com>
2022-07-16 16:27:53 -0400

    Fix Build Errors

--- src/app/patient-details/patient-details.component.ts
+++ src/app/patient-details/patient-details.component.ts
@@ -1,169 +1,109 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { PatientService } from '../Service/patient.service';
 import { Country } from '../validators/country.model';
-import { ParentErrorStateMatcher, PasswordValidator } from '../validators/password.validator';
+import {
+  ParentErrorStateMatcher,
+  PasswordValidator,
+} from '../validators/password.validator';
 import { PhoneValidator } from '../validators/phone.validator';
 import { UsernameValidator } from '../validators/username.validator';
 import { Patient } from '../Models/patient';
 import { QuestionnaireModel } from '../Models/questionaireModel';
 
-
 @Component({
   selector: 'app-patient-details-page',
   templateUrl: './patient-details.component.html',
-  styleUrls: ['./patient-details.component.scss']
+  styleUrls: ['./patient-details.component.scss'],
 })
 export class PatientDetailsComponent implements OnInit {
-
   userDetailsForm: FormGroup;
   accountDetailsForm: FormGroup;
 
   matchingPasswordsGroup: FormGroup;
   countryPhoneGroup: FormGroup;
-  myPatient : Patient;
-  myquestionnaireModel : QuestionnaireModel;
-
+  myPatient: Patient;
+  myquestionnaireModel: QuestionnaireModel;
 
   parentErrorStateMatcher = new ParentErrorStateMatcher();
 
-  genders = [
-    "Male",
-    "Female",
-    "Other"
-  ];
-
-  countries = [
-    new Country('UY', 'Uruguay'),
-    new Country('US', 'United States'),
-    new Country('AR', 'Argentina')
-  ];
-
+  genders = ['Male', 'Female', 'Other'];
 
   validation_messages = {
-    'fullname': [
-      { type: 'required', message: 'Full name is required' }
+    fullname: [{ type: 'required', message: 'Full name is required' }],
+    bio: [
+      {
+        type: 'maxlength',
+        message: 'Bio cannot be more than 256 characters long',
+      },
     ],
-    'bio': [
-      { type: 'maxlength', message: 'Bio cannot be more than 256 characters long' },
-    ],
-    'gender': [
-      { type: 'required', message: 'Please select your gender' },
-    ],
-    'birthday': [
-      { type: 'required', message: 'Please insert your birthday' },
-    ],
-    'phone': [
+    gender: [{ type: 'required', message: 'Please select your gender' }],
+    birthday: [{ type: 'required', message: 'Please insert your birthday' }],
+    phone: [
       { type: 'required', message: 'Phone is required' },
-      { type: 'validCountryPhone', message: 'Phone incorrect for the country selected' }
-    ]
+      {
+        type: 'validCountryPhone',
+        message: 'Phone incorrect for the country selected',
+      },
+    ],
   };
 
-  account_validation_messages = {
-    'username': [
-      { type: 'required', message: 'Username is required' },
-      { type: 'minlength', message: 'Username must be at least 5 characters long' },
-      { type: 'maxlength', message: 'Username cannot be more than 25 characters long' },
-      { type: 'pattern', message: 'Your username must contain only numbers and letters' },
-      { type: 'validUsername', message: 'Your username has already been taken' }
-    ],
-    'email': [
-      { type: 'required', message: 'Email is required' },
-      { type: 'pattern', message: 'Enter a valid email' }
-    ],
-    'confirm_password': [
-      { type: 'required', message: 'Confirm password is required' },
-      { type: 'areEqual', message: 'Password mismatch' }
-    ],
-    'password': [
-      { type: 'required', message: 'Password is required' },
-      { type: 'minlength', message: 'Password must be at least 5 characters long' },
-      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number' }
-    ],
-    'terms': [
-      { type: 'pattern', message: 'You must accept terms and conditions' }
-    ]
-  }
 
-  constructor(private fb: FormBuilder, private patientService: PatientService) { }
- 
+  constructor(
+    private fb: FormBuilder,
+    private patientService: PatientService
+  ) {}
 
   ngOnInit() {
     this.createForms();
-    //this.getQuestionnaire();
-}
+  }
 
-getPatientDetails(){
-  this.patientService.patientDetails().subscribe(
-    (patient : Patient) => {
+  getPatientDetails() {
+    this.patientService.patientDetails().subscribe((patient: Patient) => {
       this.myPatient = patient;
-    }
-  );
-}
-
-createForms() {
-    // matching passwords validation
-    this.matchingPasswordsGroup = new FormGroup({
-      password: new FormControl('', Validators.compose([
-        Validators.minLength(5),
-        Validators.required,
-        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
-      ])),
-      confirm_password: new FormControl('', Validators.required)
-    }, (formGroup: FormGroup) => {
-      return PasswordValidator.areEqual(formGroup);
     });
+  }
 
+  createForms() {
     // country & phone validation
-    let country = new FormControl(this.countries[0], Validators.required);
+    let birthcountry = new FormControl(this.countries[0], Validators.required);
 
     let phone = new FormControl('', {
       validators: Validators.compose([
         Validators.required,
-        PhoneValidator.validCountryPhone(country)
-      ])
+        PhoneValidator.validCountryPhone(birthcountry),
+      ]),
     });
 
     this.countryPhoneGroup = new FormGroup({
       country: country,
-      phone: phone
+      phone: phone,
     });
 
     // user details form validations
     this.userDetailsForm = this.fb.group({
-      fullname: ['Homero Simpson', Validators.required ],
-      bio: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", Validators.maxLength(256)],
+      fullname: ['Homero Simpson', Validators.required],
+      bio: [
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+        Validators.maxLength(256),
+      ],
       birthday: ['', Validators.required],
       gender: new FormControl(this.genders[0], Validators.required),
-      countryPhone: this.countryPhoneGroup
+      countryPhone: this.countryPhoneGroup,
     });
 
-
-    // user links form validations
-    this.accountDetailsForm = this.fb.group({
-      username: new FormControl('', Validators.compose([
-       UsernameValidator.validUsername,
-       Validators.maxLength(25),
-       Validators.minLength(5),
-       Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
-       Validators.required
-      ])),
-      email: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-      ])),
-      matchingPasswords: this.matchingPasswordsGroup,
-      terms: new FormControl(false, Validators.pattern('true'))
-    })
-
   }
 
-  onSubmitAccountDetails(value: any){
+  onSubmitAccountDetails(value: any) {
     console.log(value);
   }
 
-  onSubmitUserDetails(value: any){
+  onSubmitUserDetails(value: any) {
     console.log(value);
   }
-
 }
