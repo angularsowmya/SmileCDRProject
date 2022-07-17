550828ed61762d2a565b22e1dcf15631c335ba3c
sowmya2014 <75194242+sowmya2014@users.noreply.github.com>
2022-07-16 20:26:10 -0400

    Changes made for Martial Status

--- src/app/patient-details/patient-details.component.ts
+++ src/app/patient-details/patient-details.component.ts
@@ -29,28 +29,29 @@ export class PatientDetailsComponent implements OnInit {
   countryPhoneGroup: FormGroup;
   myPatient: Patient;
   myquestionnaireModel: QuestionnaireModel;
+  genderSelection: string;
+  allergiesSelection: string;
+  //selectedAllergy: string;
+ 
 
   parentErrorStateMatcher = new ParentErrorStateMatcher();
 
   genders = ['Male', 'Female', 'Other'];
+  martialStatusOptions = ['Married', 'Single', 'Divorced'];
+  allergyOptions = ['True','False'];
+
 
   validation_messages = {
-    fullname: [{ type: 'required', message: 'Full name is required' }],
-    bio: [
+    birthCountryValidation: [{ type: 'required', message: 'Birth Country is required' }],
+    bioValidation: [
       {
         type: 'maxlength',
         message: 'Bio cannot be more than 256 characters long',
       },
     ],
-    gender: [{ type: 'required', message: 'Please select your gender' }],
-    birthday: [{ type: 'required', message: 'Please insert your birthday' }],
-    phone: [
-      { type: 'required', message: 'Phone is required' },
-      {
-        type: 'validCountryPhone',
-        message: 'Phone incorrect for the country selected',
-      },
-    ],
+    genderValidation: [{ type: 'required', message: 'Please select your Gender' }],
+    maritalstatusValidation: [{ type: 'required', message: 'Please select your Marital Status' }],
+    birthDayValidation: [{ type: 'required', message: 'Please Provide your Birthday' }],
   };
 
 
@@ -70,33 +71,16 @@ export class PatientDetailsComponent implements OnInit {
   }
 
   createForms() {
-    // country & phone validation
-    let birthcountry = new FormControl(this.countries[0], Validators.required);
-
-    let phone = new FormControl('', {
-      validators: Validators.compose([
-        Validators.required,
-        PhoneValidator.validCountryPhone(birthcountry),
-      ]),
-    });
-
-    this.countryPhoneGroup = new FormGroup({
-      country: country,
-      phone: phone,
-    });
-
-    // user details form validations
+      // user details form validations
     this.userDetailsForm = this.fb.group({
-      fullname: ['Homero Simpson', Validators.required],
-      bio: [
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-        Validators.maxLength(256),
-      ],
-      birthday: ['', Validators.required],
-      gender: new FormControl(this.genders[0], Validators.required),
-      countryPhone: this.countryPhoneGroup,
+      fullName: ['Sowmya Bantupalli', Validators.required],
+      bio: ['bio', Validators.required],
+      birthCountry: ['', Validators.required],
+      birthDay: ['', Validators.required],
+      genderSelection:  ['', Validators.required],
+      maritalstatus: ['', Validators.required],   
+      allergiesSelection:  ['', Validators.required],    
     });
-
   }
 
   onSubmitAccountDetails(value: any) {
